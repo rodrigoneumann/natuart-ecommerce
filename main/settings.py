161ba935c407a3bb18afd31610774daf4daa433d
Rodrigo Neumann <rodrigoneumann@gmail.com>
2020-03-19 15:52:46 +0000

    static files config issue

--- main/settings.py
+++ main/settings.py
@@ -133,12 +133,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-if 'DEV' in env:
-    STATIC_URL = '/static/'
-else:
-    STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "staticfiles")
+    os.path.join(BASE_DIR, "static")
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+
+
+
