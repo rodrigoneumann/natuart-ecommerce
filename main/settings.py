d0722634ea6a48f35f022da3e61186cb97abc2a6
Rodrigo Neumann <rodrigoneumann@gmail.com>
2020-03-19 00:07:59 +0000

    alowed hosts hostname set

--- main/settings.py
+++ main/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["natuart.herokuapp.com"]
+ALLOWED_HOSTS = [env('HOSTNAME')]
 
 
 # Application definition
